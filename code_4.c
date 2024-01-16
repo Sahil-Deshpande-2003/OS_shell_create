1b592c0d126f68bc887ff9749430d499cc8e1c99
Sahil Deshpande <sahsdep@gmail.com>
2024-01-16 21:48:06 +0530

    Abhi PS1=w bhi chal raha hai but usse cwd pe revert hone ke baad ls karne ke baad prompt fir se \w$ hi ho ja raha hai

--- code_4.c
+++ code_4.c
@@ -45,37 +45,82 @@ int custom_execvp(char *cmd, char **args) {
     return -1;
 }
 
-
 void display_prompt(const char *custom_prompt) {
     char prompt_to_display[MAX_PATH_SIZE];
+
+    if (custom_prompt != NULL && strcmp(custom_prompt, "\\w$") == 0) {
+        // Revert to current working directory
+        display_prompt(NULL);
+        return;
+    }
 
     if (custom_prompt != NULL) {
         // Use the provided custom prompt
         snprintf(prompt_to_display, sizeof(prompt_to_display), "%s", custom_prompt);
+        // Check if the custom prompt already ends with '$', if not, append it
+        if (prompt_to_display[strlen(prompt_to_display) - 1] != '$') {
+            strcat(prompt_to_display, "$");
+        }
     } else {
         // Get the current working directory
         if (getcwd(prompt_to_display, sizeof(prompt_to_display)) == NULL) {
             perror("getcwd");
             exit(EXIT_FAILURE);
         }
+        // Append '$' to the current working directory
+        strcat(prompt_to_display, "$");
     }
 
     // Print the prompt
-    printf("%s$ ", prompt_to_display);
+    printf("%s ", prompt_to_display);
 }
 
 
+
+// void display_prompt(const char *custom_prompt) {
+//     char prompt_to_display[MAX_PATH_SIZE];
+
+//     if (custom_prompt != NULL && strcmp(custom_prompt, "$w") == 0) {
+//         // Revert to current working directory
+//         if (getcwd(prompt_to_display, sizeof(prompt_to_display)) == NULL) {
+//             perror("getcwd");
+//             exit(EXIT_FAILURE);
+//         }
+//     } else if (custom_prompt != NULL) {
+//         // Use the provided custom prompt
+//         snprintf(prompt_to_display, sizeof(prompt_to_display), "%s", custom_prompt);
+//     } else {
+//         // Get the current working directory
+//         if (getcwd(prompt_to_display, sizeof(prompt_to_display)) == NULL) {
+//             perror("getcwd");
+//             exit(EXIT_FAILURE);
+//         }
+//     }
+
+//     // Print the prompt
+//     printf("%s$ ", prompt_to_display);
+// }
+
 void set_prompt(char *new_prompt) {
-    printf("new_prompt = %s\n", new_prompt);
+    // printf("new_prompt1 = %s\n", new_prompt);
 
     if (new_prompt == NULL || strlen(new_prompt) == 0) {
         printf("Invalid PS1: Missing prompt string\n");
         return;
     }
+    // printf("new_prompt2 = %s\n", new_prompt);
+    // printf("str_check = %s\n", "\"\\w$\"");
+    // printf("bool ans = %d\n",strcmp(new_prompt,"\"\\w$\""));
+   if (strcmp(new_prompt,"\"\\w$\"") == 0){
+        // printf("Hey! I am here\n");
+        display_prompt(NULL);
+   }
+
+   else  display_prompt(new_prompt);
 
     // Directly set the prompt
     snprintf(prompt, sizeof(prompt), "%s", new_prompt); // Skip "PS1="
-    printf("Prompt set to: %s\n", prompt);
+    // printf("Prompt set to: %s\n", prompt);
 }
 
 
@@ -127,40 +172,44 @@ int main() {
     // ls < file1.txt
 
     while (1) {
-        if (fgets(input, sizeof(input), stdin) == NULL || strcmp(input, "exit\n") == 0) {
-            printf("Exiting shell...\n");
-            break;
-        }
-
-        int i = 0;
-        token = strtok(input, " \t\n");
-        while (token != NULL && i < MAX_ARG_SIZE - 1) {
-            args[i++] = token;
-            token = strtok(NULL, " \t\n");
-        }
-        args[i] = NULL;
-
-        if (i > 0) {
-            if (strcmp(args[0], "cd") == 0) {
-                if (args[1] == NULL) {
-                    printf("cd: Missing argument\n");
-                } else {
-                    if (chdir(args[1]) != 0) {
-                        perror("chdir");
-                    }
-                }
-            } else if (strncmp(args[0], "PS1=", 4) == 0) {
-                // Setting the prompt here
-                set_prompt(args[0] + 4);
-                // Display the updated prompt
-                display_prompt(prompt);
-            } else if (strncmp(args[0], "PATH=", 5) == 0) {
-                set_path(args[0] + 5);
-            } else {
-                execute_command(args);
-            }
-        }
+    if (fgets(input, sizeof(input), stdin) == NULL || strcmp(input, "exit\n") == 0) {
+        printf("Exiting shell...\n");
+        break;
     }
+
+    int i = 0;
+    token = strtok(input, " \t\n");
+    while (token != NULL && i < MAX_ARG_SIZE - 1) {
+        args[i++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    args[i] = NULL;
+
+    if (i > 0) {
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                printf("cd: Missing argument\n");
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("chdir");
+                }
+            }
+        } else if (strncmp(args[0], "PS1=", 4) == 0) {
+            // Setting the prompt here
+            set_prompt(args[0] + 4);
+        } else if (strncmp(args[0], "PATH=", 5) == 0) {
+            set_path(args[0] + 5);
+        } else {
+            execute_command(args);
+            // Display the updated prompt after executing the command
+            display_prompt(prompt);
+        }
+    } else {
+        // If no command is entered, display the prompt
+        display_prompt(prompt);
+    }
+}
+
 
     return 0;
 }
